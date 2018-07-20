59f11aa6e5e158a339d352c5d1b04c2b55fb5b3d
Mark Alaniz <markalaniz000@gmail.com>
2018-07-19 22:36:33 -0500

    Added little tweaks to the help command

--- scripts/commands.js
+++ scripts/commands.js
@@ -1,8 +1,10 @@
-exports.commands = (msg) => {
-    if (msg.content === '!help') {
-        const reply = '\nRoll Dice---------------!d <die number>' +
-        '\n\nSmug Waifu Pic---------------!smugs' +
-        '\n\nMore commands to be added later.'
-        return msg.reply(reply);
-    }
-}
+exports.commands = msg => {
+  if (msg.content === "!help") {
+    const reply =
+      "https://i.ytimg.com/vi/_vboPFgBUzI/maxresdefault.jpg" +
+      "\n\nRoll Dice---------------!d <die number>" +
+      "\n\nSmug Waifu Pic---------------!smugs" +
+      "\n\nMore commands to be added later.";
+    return msg.reply(reply);
+  }
+};
--- scripts/smug-waifu.js
+++ scripts/smug-waifu.js
@@ -5,7 +5,8 @@ exports.smug = async msg => {
     fetchSubreddit("Smugs")
       .then(urls => {
         const arrayOfURLS = urls[0].urls.slice(1);
-        const randomSmug = arrayOfURLS[Math.floor(Math.random() * arrayOfURLS.length)];
+        const randomSmug =
+          arrayOfURLS[Math.floor(Math.random() * arrayOfURLS.length)];
         return msg.reply(randomSmug);
       })
       .catch(err => {
