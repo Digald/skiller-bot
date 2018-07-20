e6f092a51d3791626fcd46a7ffc2cec5a44f51c4
Mark Alaniz <markalaniz000@gmail.com>
2018-07-20 17:33:42 -0500

    added a command line table to organize help commands

--- scripts/commands.js
+++ scripts/commands.js
@@ -1,10 +1,32 @@
+const Table = require("easy-table");
+
 exports.commands = msg => {
   if (msg.content === "!help") {
-    const reply =
-      "https://i.ytimg.com/vi/_vboPFgBUzI/maxresdefault.jpg" +
-      "\n\nRoll Dice---------------!d <die number>" +
-      "\n\nSmug Waifu Pic---------------!smugs" +
-      "\n\nMore commands to be added later.";
+    const data = [
+      {
+        command: "!d <n>",
+        name: "Roll Dice ->",
+        desc: "Get a random number between 1 & n."
+      },
+      {
+        command: "!smugs",
+        name: "Smug Waifu Picture ->",
+        desc: "Posts a random picture of about 20 of a smug faced waifu."
+      },
+      { command: "More to come..." }
+    ];
+
+    const t = new Table();
+
+    data.forEach(function(item) {
+      t.cell("Command", item.command);
+      t.cell("Name", item.name);
+      t.cell("Description", item.desc);
+      t.newRow();
+      t.newRow();
+    });
+    console.log(t.toString());
+    const reply = `https://i.ytimg.com/vi/_vboPFgBUzI/maxresdefault.jpg \n\n ${t.toString()}`;
     return msg.reply(reply);
   }
 };
