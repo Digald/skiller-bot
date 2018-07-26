4aff9a6df69ee153972e529afe969ffff234848d
Mark Alaniz <markalaniz000@gmail.com>
2018-07-25 21:34:34 -0500

    Corrected variable name in lewds.js that was the same as another variable in another file

--- scripts/lewds.js
+++ scripts/lewds.js
@@ -1,19 +1,18 @@
 const { fetchSubreddit } = require("fetch-subreddit");
-const logger = require('../logger.js').logger;
+const logger = require("../logger.js").logger;
 
 exports.lewds = async msg => {
   if (msg.content === "!lewds") {
     logger(msg);
-    if (msg.channel.name === "officer_lounge" || msg.channel.type === 'dm') {
+    if (msg.channel.name === "officer_lounge" || msg.channel.type === "dm") {
       const subreddits = ["ecchi", "Rule34LoL"];
       fetchSubreddit(subreddits)
         .then(urls => {
-          // console.log(urls);
           const arrayOfURLS = [...urls[0].urls, ...urls[1].urls.slice(1)];
           // console.log(arrayOfURLS);
-          const randomSmug =
+          const randomLewd =
             arrayOfURLS[Math.floor(Math.random() * arrayOfURLS.length)];
-          return msg.reply(randomSmug);
+          return msg.reply(randomLewd);
         })
         .catch(err => {
           console.error(err);
