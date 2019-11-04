d0606d6dd170648697bcde2c2c43a066b45cd2d5
unknown <nosracer_gt@sbcglobal.net>
2019-11-03 22:18:32 -0600

    User's id now properly gets pushed to spawn's caughtBy array

--- scripts/pokemon-catch.js
+++ scripts/pokemon-catch.js
@@ -8,36 +8,33 @@ module.exports = async msg => {
   if (getPokemon.caughtBy.indexOf(userId) !== -1) {
     return msg.reply(`You've already caught this ${getPokemon.name}.`);
   }
-  db.Spawn.update(
+  db.Spawn.updateOne(
     { name: getPokemon.name },
     { $push: { caughtBy: msg.author.id } }
-  );
-  const {
-    name,
-    pokeId,
-    evolChain,
-    types,
-    shiny,
-    spriteUrl,
-    hp,
-    atk,
-    def,
-    speed
-  } = getPokemon;
-  const spawnObject = {
-    name,
-    pokeId,
-    evolChain,
-  };
-  const userObject = {};
-  // db.User.findOneAndUpdate(
-  //   { discordId: userId },
-  //   { $push: { pokemon: spawnObject } }
-  // ).then(result => {
-  //   if (!result) {
-  //     return db.User.create(userObject);
-  //   }
-  //   return;
-  // });
+  ).exec();
+  
+  // Check if user exists
+  const user = await db.User.findOne({ discordId: userId });
+  console.log(user);
+
+  // const {
+  //   name,
+  //   pokeId,
+  //   evolChain,
+  //   types,
+  //   shiny,
+  //   spriteUrl,
+  //   hp,
+  //   atk,
+  //   def,
+  //   speed
+  // } = getPokemon;
+  // const spawnObject = {
+  //   name,
+  //   pokeId,
+  //   evolChain
+  // };
+  // const userObject = {};
+
   return msg.reply(`${getPokemon.name} has been added to your collection!`);
 };
