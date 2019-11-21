81d90e151fbb5a103f987ed8a68d0aa8a84102f7
unknown <nosracer_gt@sbcglobal.net>
2019-11-21 14:37:09 -0600

    production ready

--- scripts/pokemon-announce.js
+++ scripts/pokemon-announce.js
@@ -30,8 +30,8 @@ module.exports = client => {
       .setThumbnail(thumb)
       .setDescription("!catch to add to your collection")
       .setColor(pokeColor);
-    // client.channels.get("441820156197339136").send(embed);
-    client.users.get("129038630953025536").send(embed);
+    client.channels.get("441820156197339136").send(embed);
+    // client.users.get("129038630953025536").send(embed);
   }
   announce();
   setInterval(announce, 28800 * 1000);
