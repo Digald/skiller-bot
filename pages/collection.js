36da0ba896950d45a6e86916762b60aa749eddd6
unknown <nosracer_gt@sbcglobal.net>
2019-12-02 22:21:03 -0600

    Reworking routes for pages

--- pages/collection.js
+++ pages/collection.js
@@ -1,19 +1,13 @@
 import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
+import fetch from "isomorphic-unfetch";
+import { server } from "../lib/config";
 import { useRouter } from "next/router";
 import { withRedux } from "../lib/redux";
 import Layout from "../components/Layout";
 import PokemonGrid from "../components/PokemonGrid";
 
 const Collection = () => {
-  const router = useRouter();
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch({
-      type: "SET-USER",
-      data: router.query
-    });
-  });
   return (
     <Layout>
       <PokemonGrid />
@@ -21,7 +15,15 @@ const Collection = () => {
   );
 };
 
-Collection.getInitialProps = () => {
+Collection.getInitialProps = async ({ reduxStore , query}) => {
+  const res = await fetch(`${server}/api/user/${query.userID}`);
+  const json = await res.json();
+  const { dispatch } = reduxStore;
+  console.log(json);
+  dispatch({
+    type: "SET-USER",
+    data: json
+  });
   return {};
 };
 
--- routes/routes.js
+++ routes/routes.js
@@ -2,23 +2,6 @@ const db = require("../models");
 
 module.exports = (server, handle, app) => {
   // 129038630953025536 my discord id
-  server.get("/collection/:discordID", (req, res) => {
-    db.User.findOne({
-      discordId: req.params.discordID
-    }).then(result => {
-      if (!result) {
-        return app.render(req, res, "/404");
-      }
-      // Sort pokemon by their Id number from Gen 1 to Gen 8
-      const pokeArr = result.pokemon;
-      pokeArr.sort((a, b) => {
-        return parseInt(a.pokeId) - parseInt(b.pokeId);
-      });
-      result.pokemon = pokeArr;
-      return app.render(req, res, "/collection", result);
-    });
-  });
-
   server.get("/api/users", async (req, res) => {
     // Need to omit user tokens after team battles are implemented.
     db.User.find().then(result => {
@@ -27,7 +10,30 @@ module.exports = (server, handle, app) => {
     return;
   });
 
-  server.all("/", (req, res) => {
+  server.get("/api/user/:discordID", (req, res) => {
+    db.User.findOne({
+      discordId: req.params.discordID
+    }).then(result => {
+      if (!result) {
+        return res.json();
+      }
+      // Sort pokemon by their Id number from Gen 1 to Gen 8
+      const pokeArr = result.pokemon;
+      pokeArr.sort((a, b) => {
+        return parseInt(a.pokeId) - parseInt(b.pokeId);
+      });
+      result.pokemon = pokeArr;
+      return res.json();
+    });
+  });
+
+  server.get("/collection/:discordID", (req, res) => {
+    return app.render(req, res, "/collection", {
+      userID: req.params.discordID
+    });
+  });
+
+  server.get("/", (req, res) => {
     return app.render(req, res, "/index");
   });
 
