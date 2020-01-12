44b344ba592e081573784b4ebe98a13c9a98b303
unknown <nosracer_gt@sbcglobal.net>
2020-01-11 19:52:30 -0600

    changed paths to get images

--- scripts/pokemon-announce.js
+++ scripts/pokemon-announce.js
@@ -31,7 +31,7 @@ module.exports = client => {
       .setDescription("!catch to add to your collection")
       .setColor(pokeColor);
     // ***PRODUCTION***
-    client.channels.get("441820156197339136").send(embed);
+    // client.channels.get("441820156197339136").send(embed);
     // ***DEVELOPMENT***
     // client.users.get("129038630953025536").send(embed);
   }
